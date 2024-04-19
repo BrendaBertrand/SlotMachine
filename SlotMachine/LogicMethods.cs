35e8bc401164b718fbb0323d5a154baed20b4c1c
Brenda Bertrand <brenda.bertrand.b@gmail.com>
2024-04-19 09:33:39 +0200

    Add of GlobalTurn()

--- SlotMachine/LogicMethods.cs
+++ SlotMachine/LogicMethods.cs
@@ -184,4 +184,51 @@ public class LogicMethods
         UIMethods.ClearUI();
         return creditAccount;
     }
+
+    public static void GlobalTurn(double creditAccount)
+    {
+        do
+        {
+            char userChoice = UIMethods.GetMenuChoice(creditAccount);
+            bool isGameOn = UIMethods.CheckEndOfGame(userChoice, creditAccount);
+
+            if (!isGameOn)
+            {
+                break;
+            }
+
+            bool isCorrectChoice = UIMethods.CheckUserChoice(userChoice, creditAccount);
+
+            if (!isCorrectChoice)
+            {
+                continue;
+            }
+
+            creditAccount -= Constants.gameDefinition[userChoice];
+
+            int[,] slotArray = LogicMethods.ArrayGenerator();
+
+            // Check for Jackpot
+            if (LogicMethods.CheckJackpot(slotArray))
+            {
+                creditAccount = LogicMethods.HandleJackpot(creditAccount, slotArray);
+                continue;
+            }
+
+            // Add of credit
+            if (userChoice == Constants.ADD_MONEY)
+            {
+                creditAccount = LogicMethods.AddCredit(creditAccount,
+                    "\nHow much credit do you want to add to your account ?\n");
+                continue;
+            }
+
+            // Check for gain
+            double gain = LogicMethods.CheckForGain(userChoice, slotArray);
+            creditAccount += gain;
+
+            UIMethods.DisplayArray(slotArray);
+            UIMethods.DisplayMessage(gain > 0 ? $"You Won {gain} credit!\n" : "Try again\n");
+        } while (true);
+    }
 }
